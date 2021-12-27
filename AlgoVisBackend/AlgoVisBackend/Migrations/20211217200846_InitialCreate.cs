ebe6cf5f7b4dd8a0ab6dcaabf4157ddfa693c43d
Kamil Niemiec <ksonax@gmail.com>
2021-12-27 14:25:53 +0100

    Added ASP.NET Core backend

---
+++ AlgoVisBackend/AlgoVisBackend/Migrations/20211217200846_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlgoVisBackend.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Algorithms",
+                columns: table => new
+                {
+                    AlgorithmId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AlgorithmName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JavaScriptImplementation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Algorithms", x => x.AlgorithmId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Algorithms");
+        }
+    }
+}
---
+++ AlgoVisBackend/AlgoVisBackend/Models/AlgorithmContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlgoVisBackend.Models
+{
+    public class AlgorithmContext:DbContext
+    {
+        public AlgorithmContext(DbContextOptions<AlgorithmContext> options):base(options)
+        {
+                       
+        }
+        public DbSet<Algorithm> Algorithms { get; set; }
+    }
+}
